fb6f270dda118dd5e226d36e44f67e84d85315f2
TheJosephGT <Joseph_Garcia@ucne.edu.do>
2024-04-03 22:36:09 -0400

    add modal

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
+import ModalProyect from "./ModalProyect";
 import React from "react";
 
 function Dashboard() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       {/* Content Header (Page header) */}
@@ -92,7 +98,7 @@ function Dashboard() {
         <div className="container-fluid">
           <div className="row mb-2">
             <div className="col-sm-6">
-              <h1>Projects</h1>
+              <h1>Proyectos</h1>
             </div>
           </div>
         </div>
@@ -101,34 +107,16 @@ function Dashboard() {
       <section className="content">
         <div className="card">
           <div className="card-header">
-            <h3 className="card-title">Projects</h3>
-            <div className="card-tools">
-              <button
-                type="button"
-                className="btn btn-tool"
-                data-card-widget="collapse"
-                title="Collapse"
-              >
-                <i className="fas fa-minus" />
-              </button>
-              <button
-                type="button"
-                className="btn btn-tool"
-                data-card-widget="remove"
-                title="Remove"
-              >
-                <i className="fas fa-times" />
-              </button>
-            </div>
+            <h1 className="card-title">Proyectos</h1>
           </div>
           <div className="card-body p-0">
             <table className="table table-striped projects">
               <thead>
                 <tr>
                   <th style={{ width: "1%" }}>#</th>
-                  <th style={{ width: "20%" }}>Project Name</th>
+                  <th style={{ width: "20%" }}>Nombre Proyecto</th>
                   <th style={{ width: "30%" }}>Fecha</th>
-                  <th>Project Progress</th>
+                  <th>Progreso</th>
                   <th style={{ width: "8%" }} className="text-center">
                     Status
                   </th>
@@ -165,103 +153,20 @@ function Dashboard() {
                     <span className="badge badge-success">Success</span>
                   </td>
                   <td className="project-actions text-right">
-                    <a className="btn btn-primary btn-sm" href="#">
+                    <button
+                      className="btn btn-primary btn-sm"
+                      onClick={handleShow}
+                    >
                       <i className="fas fa-folder"></i>
-                      View
-                    </a>
+                      Ver
+                    </button>
                     <a className="btn btn-info btn-sm" href="#">
                       <i className="fas fa-pencil-alt"></i>
-                      Edit
+                      Editar
                     </a>
                     <a className="btn btn-danger btn-sm" href="#">
                       <i className="fas fa-trash"></i>
-                      Delete
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>#</td>
-                  <td>
-                    <a style={{ fontSize: 18 }}>Nombre Proyecto</a>
-                  </td>
-                  <td>
-                    <ul className="list-inline">
-                      <li className="list-inline-item">
-                        <a style={{ fontSize: 18 }}>Created 01.01.2019</a>
-                      </li>
-                    </ul>
-                  </td>
-                  <td className="project_progress">
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-green"
-                        role="progressbar"
-                        aria-valuenow={57}
-                        aria-valuemin={0}
-                        aria-valuemax={100}
-                        style={{ width: "57%" }}
-                      ></div>
-                    </div>
-                    <small>57% Complete</small>
-                  </td>
-                  <td className="project-state">
-                    <span className="badge badge-success">Success</span>
-                  </td>
-                  <td className="project-actions text-right">
-                    <a className="btn btn-primary btn-sm" href="#">
-                      <i className="fas fa-folder"></i>
-                      View
-                    </a>
-                    <a className="btn btn-info btn-sm" href="#">
-                      <i className="fas fa-pencil-alt"></i>
-                      Edit
-                    </a>
-                    <a className="btn btn-danger btn-sm" href="#">
-                      <i className="fas fa-trash"></i>
-                      Delete
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>#</td>
-                  <td>
-                    <a style={{ fontSize: 18 }}>Nombre Proyecto</a>
-                  </td>
-                  <td>
-                    <ul className="list-inline">
-                      <li className="list-inline-item">
-                        <a style={{ fontSize: 18 }}>Created 01.01.2019</a>
-                      </li>
-                    </ul>
-                  </td>
-                  <td className="project_progress">
-                    <div className="progress progress-sm">
-                      <div
-                        className="progress-bar bg-green"
-                        role="progressbar"
-                        aria-valuenow={57}
-                        aria-valuemin={0}
-                        aria-valuemax={100}
-                        style={{ width: "57%" }}
-                      ></div>
-                    </div>
-                    <small>57% Complete</small>
-                  </td>
-                  <td className="project-state">
-                    <span className="badge badge-success">Success</span>
-                  </td>
-                  <td className="project-actions text-right">
-                    <a className="btn btn-primary btn-sm" href="#">
-                      <i className="fas fa-folder"></i>
-                      View
-                    </a>
-                    <a className="btn btn-info btn-sm" href="#">
-                      <i className="fas fa-pencil-alt"></i>
-                      Edit
-                    </a>
-                    <a className="btn btn-danger btn-sm" href="#">
-                      <i className="fas fa-trash"></i>
-                      Delete
+                      Eliminar
                     </a>
                   </td>
                 </tr>
@@ -270,6 +175,7 @@ function Dashboard() {
           </div>
         </div>
       </section>
+      <ModalProyect show={show} handleClose={handleClose} />
     </>
   );
 }
